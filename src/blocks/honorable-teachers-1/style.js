dc9be512620c5542154d3c6e6a87b430e12aa8f0
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-06-30 00:04:18 +0600

    honorable teachers 1

--- src/blocks/honorable-teachers-1/style.js
+++ src/blocks/honorable-teachers-1/style.js
@@ -58,44 +58,74 @@ export default function Style({attributes, wrapper, sheet}){
             top: 0;
         }
 
-        ${wrapper} .testimonial-slides .slick-slide:before{
-            content: "";
-            border: 2px solid #003646;
-            border-radius: 50%;
-            display: block;
-            height: 250px;
-            opacity: 0;
-            pointer-events: none;
-            position: absolute;
-            transform: translate(-21px,-17px);
-            transition: .3s;
-            width: 250px;
-            tranistion: all linear .3s;
-        }
-
-        ${wrapper} .testimonial-slides .slick-slide.slick-current:before {
-            opacity: 1;
-        }
-
-        ${wrapper} .testimonial-slides .slick-slide.slick-current img{
-            width: 220px;
-            height: 220px;
-           
+        ${wrapper} .testimonial-slides .slick-slide img {
+            display: inline-block;
         }
 
         ${wrapper} .testimonial-slides .slick-slide:first-child {
-            right: 20px;
+            right: 44px;
             top: 257px;
         }
 
+        ${wrapper} .testimonial-slides .slick-slide:first-child:before {
+            content: "";
+            border: 2px solid #003646;
+            border-radius: 50%;
+            opacity: 0;
+            pointer-events: none;
+            position: absolute;
+            transform: translate(-82px,-12px);
+            width: 160px;
+            height: 160px;
+            transition: all ease .2s
+        }
+
+        ${wrapper} .testimonial-slides .slick-slide.slick-current:first-child:before {
+            opacity: 1;
+        }
+
         ${wrapper} .testimonial-slides .slick-slide:nth-child(2) {
-            right: 42px;
+            right: 65px;
             top: 13px;
+        }
+
+        ${wrapper} .testimonial-slides .slick-slide:nth-child(2):before {
+            content: "";
+            border: 2px solid #003646;
+            border-radius: 50%;
+            opacity: 0;
+            pointer-events: none;
+            position: absolute;
+            transform: translate(-82px,-12px);
+            width: 160px;
+            height: 160px;
+            transition: all ease .2s
+        }
+
+        ${wrapper} .testimonial-slides .slick-slide.slick-current:nth-child(2):before {
+            opacity: 1;
         }
 
         ${wrapper} .testimonial-slides .slick-slide:nth-child(3) {
             left: 67px;
             top: 188px;
+        }
+
+        ${wrapper} .testimonial-slides .slick-slide:nth-child(3):before {
+            content: "";
+            border: 2px solid #003646;
+            border-radius: 50%;
+            opacity: 0;
+            pointer-events: none;
+            position: absolute;
+            transform: translate(-127px,-21px);
+            width: 250px;
+            height: 250px;
+            transition: all ease .2s
+        }
+
+        ${wrapper} .testimonial-slides .slick-slide.slick-current:nth-child(3):before {
+            opacity: 1;
         }
     `
 }
