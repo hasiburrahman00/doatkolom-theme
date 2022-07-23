2196dfeb68433c30c587883b9b89947ab89e0151
Ashraf Mollah <mollah.dev@gmail.com>
2022-07-24 01:42:56 +0600

    optimised navbar comonent with memo

--- src/admin/context/index.js
+++ src/admin/context/index.js
@@ -1,28 +1,24 @@
-import { createContext, useEffect } from "react"; 
+import { createContext, useEffect, useState } from "react"; 
 import { SnackbarProvider } from 'notistack';
 
 import AdminPageSkeleton from "../../skeleton/admin-page";
 import useStore from "./../../utils/store";
 import API from './../api';
-import Navbar from "../components/Navbar";
-
-const $ = window.jQuery;
+import MemoedNavbar from "../components/Navbar";
 
 export const AdminContext = createContext(null);
 export function AdminContextProvider(props) {
 
-    // Global state
-    const [ attribute, setAttribute ] = useStore({
-        site_name   : 'DoatKolom',
-        version     : '1.0.0',
-        logo        : doatkolom_object.img + 'logo.webp',
-        save_setting_loader: false,
-        header_css: {},
+    // for global state
+    const [ attribute, setAttribute ] = useStore({})
+
+    // for navbar component
+    const [ saveButton, setSaveButton ] = useState({
+        text: 'Save Now',
+        loading: false
     })
 
     useEffect(()=>{
-
-        $(document).trigger( 'wp-window-resized' );
 
         if( !attribute.setting_fields ) {
             API.setting_fields().then(res => {
@@ -32,34 +28,10 @@ export function AdminContextProvider(props) {
 
     },[])
 
-    $(document).on( 'wp-menu-state-set wp-collapse-menu', function( event, eventData ) {
-        if( eventData.state === 'open' || eventData.state === 'folded' ) {
-            const sidebarWidth      = $('#adminmenuwrap').outerWidth();
-            const adminBarHeight    = $('#wpadminbar').outerHeight();
-            setAttribute({
-                header_css: {
-                    left: sidebarWidth + 'px',
-                    top: adminBarHeight + 'px',
-                    width: `calc(100% - ${sidebarWidth}px)`
-                }
-            })
-        }
-
-        if( eventData.state === 'responsive' ) {
-            setAttribute({
-                header_css: {
-                    left: 0,
-                    top: 0,
-                    width: `100%`
-                }
-            })
-        }
-    });
-
     return(
         <AdminContext.Provider value={{attribute, setAttribute}}>
             <SnackbarProvider maxSnack={10}>
-                <Navbar menu={props.menu}/>
+                <MemoedNavbar saveButton={saveButton} setSaveButton={setSaveButton} menu={props.menu}/>
                 <main className="mt-28">
                     { attribute.setting_fields ? props.children : <AdminPageSkeleton/> }
                 </main>
