96894871a3300245f8809ba12d97a290b3acb6ef
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-29 00:00:02 +0600

    box sliders

--- src/blocks/box-slider-1/edit.js
+++ src/blocks/box-slider-1/edit.js
@@ -39,7 +39,7 @@ export default function Edit({ attributes, setAttributes}) {
             <div className="box-slider">
               {
                 attributes.box_slider?.map((item,index)=>(
-                  <div key={index} className="text-center w-48 rounded-md bg-white py-5 border border-gray-100 border-solid">
+                  <div key={index} className="text-center max-w-48 rounded-md bg-white py-5 border border-gray-100 border-solid">
                     <picture>
                       <img width="50" height="60" className="object-cover" src={item.background} alt="service-image" />
                     </picture>
@@ -48,12 +48,14 @@ export default function Edit({ attributes, setAttributes}) {
                 ))
               }       
             </div>
-              <button className="slick-prev-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] right-[-65px] translate-y-[-50%] z-10 hidden lg:block">
-                  <PrevArrow/>
-              </button>
-              <button className="slick-next-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] left-[-65px] translate-y-[-50%] z-10 hidden lg:block">
-                  <NextArrow/>
-              </button>
+            <div className="arrows hidden">
+              <button className="slick-prev-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] right-[-65px] translate-y-[-50%] z-10">
+                    <PrevArrow/>
+                </button>
+                <button className="slick-next-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] left-[-65px] translate-y-[-50%] z-10">
+                    <NextArrow/>
+                </button>
+            </div>
           </div>
       </div>
 	);
