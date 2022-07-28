4d03504550265e933557d1fb021ff308d6e73258
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-28 21:39:40 +0600

    box slider

--- src/blocks/box-slider-1/edit.js
+++ src/blocks/box-slider-1/edit.js
@@ -1,3 +1,5 @@
+import { PrevArrow,NextArrow } from "./icons";
+
 export default function Edit({ attributes, setAttributes}) {
 
     wp.element.useEffect(()=>{
@@ -32,19 +34,27 @@ export default function Edit({ attributes, setAttributes}) {
     },[])
 
 	return (
-		<div className="max-w-screen-xl lg:px-8 px-5 mx-auto py-12 md:py-10">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5">
-                {
-                    attributes.box_slider?.map((item,index)=>(
-                        <div key={index} className="mb-1 text-center rounded-lg p-4">
-                            <picture>
-                                <img width="50" height="60" className="object-cover" src={item.background} alt="service-image" />
-                            </picture>
-                            <p className="m-0 mt-3 text-base font-secondary font-normal text-primary">{item.text}</p>
-                        </div>
-                    ))
-                }
+		  <div className="max-w-screen-xl lg:px-8 px-5 mx-auto py-12 md:py-10">
+          <div className="main-box-slider relative">
+            <div className="box-slider">
+              {
+                attributes.box_slider?.map((item,index)=>(
+                  <div key={index} className="text-center w-48 rounded-md bg-white py-5 border border-gray-100 border-solid">
+                    <picture>
+                      <img width="50" height="60" className="object-cover" src={item.background} alt="service-image" />
+                    </picture>
+                    <p className="center-text transition ease-linear duration-300 m-0 mt-3 text-lg font-secondary font-bold text-black-90">{item.text}</p>
+                  </div>
+                ))
+              }       
             </div>
-        </div>
+              <button className="slick-prev-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] right-[-65px] translate-y-[-50%] z-10 hidden lg:block">
+                  <PrevArrow/>
+              </button>
+              <button className="slick-next-btn w-[50px] h-[50px] rounded-full border-none bg-red-100 hover:bg-red-500 transition ease-linear duration-150 cursor-pointer absolute top-[25%] left-[-65px] translate-y-[-50%] z-10 hidden lg:block">
+                  <NextArrow/>
+              </button>
+          </div>
+      </div>
 	);
 }
