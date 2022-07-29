dea4efd331ab06ec4e5a3cf0e7fa96d02322c235
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-30 00:49:48 +0600

    number box 1

--- src/blocks/about-2/save.js
+++ src/blocks/about-2/save.js
@@ -7,7 +7,10 @@ export default function Save({ attributes }) {
                         <h3 className="m-0 mb-3 text-primary font-secondary text-xl font-weight_secondary">{attributes.top_heading}</h3>
                         <h1 className="m-0 mb-4 font-secondary font-weight_secondary text-black text-5xl leading-[70px] pr-20">{attributes.heading}</h1>
                         <p className="m-0 text-black font-secondary font-weight_primary text-lg pr-7">{attributes.description}</p>
-                        <a href="#" className="bg-primary text-white hover:bg-primary_dark hover:text-white py-3 px-10 rounded-md inline-block no-underline mt-10">{attributes.button.text}</a>
+                        {
+                            attributes.button.visible &&
+                            <a href={attributes.button.url} className="bg-primary text-white hover:bg-primary_dark hover:text-white py-3 px-10 rounded-md inline-block no-underline mt-10">{attributes.button.text}</a>
+                        }
                     </div>
                 <div className="overflow-hidden rounded-md mt-20 h-[386px]">
                     <picture>
