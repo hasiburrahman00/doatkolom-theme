d95f5cd30533dc2ec563b679e61d9aa86b0ac664
Ashraf Mollah <mollah.dev@gmail.com>
2022-07-29 23:20:32 +0600

    added gallery page api

--- src/admin/components/PictureOption.js
+++ src/admin/components/PictureOption.js
@@ -1,10 +1,24 @@
-import SettingsIcon from "../icons/settings"
-export default function PictureOption() {
+import Update from "../icons/update"
+import Trash from "../icons/trash"
+import Select from "../icons/select"
+export default function PictureOption(props) {
+
+    const clickHandler = action => {
+        if( props.onClick && typeof props.onClick === 'function' ) {
+            props.onClick(action)
+        }
+    }
+
     return (
-        <span className="absolute h-[30px] items-center bottom-[15px] left-[15px] w-[calc(100%_-_30px)] bg-[#fff]/80 flex justify-between rounded-md">
-            <span className="h-[30px] rounded-tl-md rounded-bl-md flex items-center justify-center text-center w-full hover:bg-white cursor-pointer">Select</span>
-            <span className="h-[30px] border-l border-solid border-[#ddd] border-y-0 border-r-0 px-3 flex items-center rounded-tr-md rounded-br-md cursor-pointer hover:bg-white">
-                <SettingsIcon className="w-4"/>
+        <span className="absolute hidden bottom-[5px] left-[5px] w-[calc(100%_-_10px)] group-hover:flex justify-center items-center">
+            <span onClick={() => clickHandler('mark')} className="h-[30px] bg-[#fff]/80 px-3 flex items-center rounded-tl-md rounded-bl-md cursor-pointer hover:bg-white">
+                <Select className="w-4"/>
+            </span>
+            <span onClick={() => clickHandler('update')} className="h-[30px] border-l bg-[#fff]/80 border-solid border-[#ddd] border-y-0 border-r-0 px-3 flex items-center cursor-pointer hover:bg-white">
+                <Update className="w-4"/>
+            </span>
+            <span onClick={() => clickHandler('delete')} className="h-[30px] bg-[#fff]/80 border-l bg-[#fff].80 border-solid border-[#ddd] border-y-0 border-r-0 px-3 flex items-center rounded-tr-md rounded-br-md cursor-pointer hover:bg-white">
+                <Trash className="w-4"/>
             </span>
         </span>
     )
--- src/admin/pages/gallery.js
+++ src/admin/pages/gallery.js
@@ -4,20 +4,69 @@ import Panel from "../layouts/panel";
 import SelectMultiImage from "../components/SelectMultiImage";
 import PictureOption from "../components/PictureOption";
 import API from "../api";
-import { useState } from 'react';
+
+import { AdminContext } from '../context';
+import { useEffect, useContext } from 'react';
+import { useSnackbar } from 'notistack';
 
 export default function Gallery() {
 
-    const [ loading, setLoading ] = useState(false);
-    const image = doatkolom_object.img + 'gallery-img.webp'
+    const { attribute, setAttribute, loading, setLoading } = useContext(AdminContext)
+    
+    const { enqueueSnackbar } = useSnackbar()
 
-    const imageSelectHandler = ({id, url}) => {
+    useEffect(()=>{
+        if( !attribute.galleries ) {
+            setLoading(true);
+            API.galleries().then( res => {
+                setAttribute({
+                    galleries: res.data,
+                    galleries_mark: new Set(),
+                })
+                setLoading(false)
+            })            
+        }
+    },[])
+
+    const imageSelectHandler = ({id, data}) => {
         setLoading(true)
         API.add_gallery({image_ids: id}).then( res => {
             setLoading(false)
-            console.log(res)
-
+            setAttribute({galleries : [
+                ...data,
+                ...attribute.galleries
+            ]})
+            enqueueSnackbar(res.message, { variant: res.status } )
         })
+    }
+    const updateState = () => setAttribute({galleries_mark: attribute.galleries_mark})
+    const imageAction = {
+        mark() {
+            const { image_id } = this;
+            attribute.galleries_mark.has( image_id ) ? attribute.galleries_mark.delete(image_id) : attribute.galleries_mark.add(image_id);
+            updateState();            
+        },
+
+        update() {
+
+        },
+
+        delete() {
+            const { image_id, index } = this;
+            setLoading(true)
+            API.delete_gallery({image_ids: [image_id]}).then( res => {
+                attribute.galleries.splice(index, 1);
+                if( attribute.galleries_mark.has(image_id) ) {
+                    attribute.galleries_mark.delete(image_id)
+                }
+                enqueueSnackbar(res.message, { variant: res.status } )
+                setAttribute({
+                    galleries: attribute.galleries,
+                    galleries_mark: attribute.galleries_mark
+                })
+                setLoading(false)
+            })
+        }
     }
 
     return (
@@ -26,42 +75,18 @@ export default function Gallery() {
             <Panel>
                 <Panel.VideoHeader url="#">Photo Gallery</Panel.VideoHeader>
                 <Panel.Body>
+                    
                     <div className="grid grid-cols-6 gap-5">
-                        <div className="bg-[#eee] hover:bg-[#ddd] cursor-pointer rounded-md flex items-center justify-center border border-solid border-[#ddd]">
+                        <div className="bg-[#eee] h-[145px] hover:bg-[#ddd] cursor-pointer rounded-md flex items-center justify-center border border-solid border-[#ddd]">
                             <SelectMultiImage disabled={loading} onSelect={imageSelectHandler}/>
                         </div>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
-                        <picture className="relative">
-                            <img className="lazyload w-full rounded-md object-cover border border-solid border-[#ddd]" data-src={image} alt="gallery image"/>
-                            <PictureOption/>
-                        </picture>
+                        { attribute.galleries?.map((item, index) => (
+                            <picture className={`relative group border border-solid rounded-md ${attribute.galleries_mark.has(item.image_id) ? 'border-[#1976d2] grayscale-element': 'border-[#ddd]'}`} key={index}>
+                                <img loading="lazy" width="140" height="145" className="w-full rounded-md object-cover -mb-1" src={item.image_url} alt="gallery image"/>
+                                <PictureOption onClick={ action => imageAction[action].call({...item, index})}/>
+                            </picture>
+                        )) }
                     </div>
-                    <ul className="pagination">
-                        <li></li>
-                    </ul>
                 </Panel.Body>
             </Panel>
         </PageWrapper>
