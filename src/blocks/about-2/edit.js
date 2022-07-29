487343c9719b664d382fe6b7fb2bd6f1d8fa29cb
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-30 00:16:04 +0600

    about 2

--- src/blocks/about-2/edit.js
+++ src/blocks/about-2/edit.js
@@ -6,7 +6,6 @@ export default function Edit({ attributes, setAttributes}) {
         }
     },[])
 
-
 	return (
 		<div className="max-w-screen-xl px-5 py-14 mx-auto md:py-28">
             <div className="grid grid-cols-2 gap-5">
@@ -16,9 +15,9 @@ export default function Edit({ attributes, setAttributes}) {
                     <p className="m-0 text-black font-secondary font-weight_primary text-lg pr-7">{attributes.description}</p>
                     <a href="#" className="bg-primary text-white hover:bg-primary_dark hover:text-white py-3 px-10 rounded-md inline-block no-underline mt-10">{attributes.button.text}</a>
                 </div>
-                <div className="overflow-hidden rounded-md mt-20">
+                <div className="overflow-hidden rounded-md mt-20 h-[386px]">
                     <picture>
-                        <img width="646" height="386" className="w-full object-cover" src={attributes.institution_image} alt="" />
+                        <img width="646" height="386" className="w-full h-[386px] object-cover" src={attributes.institution_image} alt="institution-image" />
                     </picture>
                 </div>
             </div>           
