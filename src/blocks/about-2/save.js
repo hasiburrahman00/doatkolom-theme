945e62c1adeac86a4b912c055afd1f9ec228e001
Ashraf Mollah <mollah.dev@gmail.com>
2022-07-30 16:42:38 +0600

    fixed about 2 issues

--- src/blocks/about-2/save.js
+++ src/blocks/about-2/save.js
@@ -1,22 +1,22 @@
 export default function Save({ attributes }) {
 	return (
-        <div className="max-w-screen-xl px-5 py-14 mx-auto md:py-28">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        <div className="max-w-screen-xl px-5 py-8 mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 items-start gap-5">
                 <div>
-                    <h3 className="m-0 mb-3 text-primary font-secondary text-xl font-weight_secondary">{attributes.top_heading}</h3>
-                    <h1 className="m-0 mb-4 font-secondary font-weight_secondary text-black text-3xl md:text-4xl lg:text-5xl leading-[50px] md:leading-[60px] lg:leading-[70px_!important]"
+                    <h3 className="m-0 mb-3 text-sub_title font-secondary text-xl font-weight_secondary">{attributes.top_heading}</h3>
+                    <h1 className="m-0 mb-4 font-primary font-weight_tertiary text-title text-3xl md:text-4xl lg:text-5xl leading-[50px] md:leading-[60px] lg:leading-[70px_!important]"
                      dangerouslySetInnerHTML={{__html: attributes.heading}}
                     />
-                    <p className="m-0 text-black font-secondary font-weight_primary text-base lg:text-lg lg:pr-7">{attributes.description}</p>
+                    <p className="m-0 text-title_description font-secondary font-weight_primary text-base lg:text-lg lg:pr-7">{attributes.description}</p>
                     {
                         attributes.button.visible &&
-                        <a href="#" className="bg-primary text-white hover:bg-primary_dark hover:text-white py-3 px-10 rounded-md inline-block no-underline mt-10 text-base lg:text-lg">{attributes.button.text}</a>
+                        <a href="#" className="bg-primary text-white hover:bg-primary_dark hover:text-white py-3 px-10 primary-shadow transition duration-200 ease-linear rounded-md inline-block no-underline mt-10 text-base lg:text-lg">{attributes.button.text}</a>
                     }
                    
                 </div>
-                <div className="overflow-hidden rounded-md mt-7 md:mt-10 lg:mt-20 h-[386px]">
+                <div className="overflow-hidden rounded-md -order-1 md:order-1">
                     <picture>
-                        <img width="646" height="386" className="w-full h-[386px] object-cover" src={attributes.institution_image} alt="institution-image" />
+                        <img width="646" height="386" className="w-full object-cover lazyload" data-src={attributes.institution_image} alt="institution-image" />
                     </picture>
                 </div>
             </div>           
