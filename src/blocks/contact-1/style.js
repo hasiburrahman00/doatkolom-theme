5b7b1547edad792cb0e1cfbb97dac6dd71923341
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-31 14:30:12 +0600

    contact page

--- src/blocks/contact-1/style.js
+++ src/blocks/contact-1/style.js
@@ -1,12 +1,3 @@
 export default function Style({attributes, wrapper}){
-    return  `
-        ${wrapper} .form-field input {
-            background: var(--doatkolom_light_gray_color);
-            border: none;
-        }
-
-        ${wrapper} .form-field input:focus {
-            border: 1px solid var(--doatkolom_secondary_color) 
-        }
-    `
+    return  ``
 }
