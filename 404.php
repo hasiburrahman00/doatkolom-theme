0bcb215511a8bef3708d732eb8605f027b601d72
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-31 17:50:52 +0600

    404 page issue fixed

--- 404.php
+++ 404.php
@@ -11,14 +11,14 @@ get_header();
 ?>
     <div class="bg-primary_light relative">
         <div class="max-w-screen-xl text-center lg:px-8 px-5 mx-auto">
-            <img class="w-[100%] sm:w-[auto]" src="https://i.ibb.co/GMVq6zC/Rectangle-452.png" alt="Rectangle-452" border="0">
+            <img class="w-[100%] sm:w-[auto] lazyload" data-src="https://i.ibb.co/GMVq6zC/Rectangle-452.png" alt="Rectangle-452" border="0">
             <h1 class="font-primary font-weight_tartiary text-3xl sm:text-4xl lg:text-5xl px-5 xl:px-[312px] leading-[40px] md:leading-[60px] mt-28 pb-16"><span class="text-primary">Ooops!</span> You weren't supposed to see this</h1>
             <a href="#" class="font-secondary bg-primary no-underline px-6 py-3 rounded-md text-lg font-weight_primary text-white inline-flex items-center mb-32 hover:bg-primary_dark transition ease-linear duration-150">
-            <img src="https://i.ibb.co/KLqWJ99/Arrow-1.png" alt="Arrow-1" border="0">    
-            <span class="ml-2">Go Back To Home</span>
+            <img data-src="https://i.ibb.co/KLqWJ99/Arrow-1.png" alt="Arrow-1" border="0">    
+            <span class="ml-2 lazyload">Go Back To Home</span>
             </a>
         </div>
-        <img class="hidden md:block absolute bottom-[-2px] left-0" src="https://i.ibb.co/18tGHf1/bottomleft-1.png" alt="bottomleft-1" border="0">
+        <img class="hidden md:block absolute bottom-[-2px] left-0 lazyload" data-src="https://i.ibb.co/18tGHf1/bottomleft-1.png" alt="bottomleft-1" border="0">
     </div>
 <?php 
 get_footer();
