241915f97d42cabd9b153b5c92273cab589ee356
Azmir Uddin Alif <79633608+Azmir02@users.noreply.github.com>
2022-07-31 22:59:26 +0600

    arrow issue fixed

--- src/blocks/image-slider-1/icons.js
+++ src/blocks/image-slider-1/icons.js
@@ -4,7 +4,7 @@ export function NextArrow () {
             <mask id="mask0_415_1906" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
                 <rect x="24" y="24" width="24" height="24" transform="rotate(180 24 24)" fill="#D9D9D9"/>
             </mask>
-            <g mask="url(#mask0_415_1906)">
+            <g>
                 <path d="M10.1946 6L16.2045 12L10.1946 18L8.79235 16.6L13.3999 12L8.79235 7.4L10.1946 6Z" fill="currentColor"/>
             </g>
         </svg>
