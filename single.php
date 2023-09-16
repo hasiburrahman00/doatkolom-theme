42e601549bfb323a8acb24f875fbf5574bbb107b
Ashraf Mollah <mollah.dev@gmail.com>
2023-09-16 21:27:37 +0600

    added single page css

--- single.php
+++ single.php
@@ -13,11 +13,11 @@ View::part('blog/single-banner');
 ?>
 <main class="pt-10 pb-16 md:pt-14 md:pb-20">
     <div class="container lg:px-16">
-        <div class="grid md:grid-cols-[1fr_365px] gap-10">
-            <div>
+        <div class="grid md:grid-cols-[1fr_300px] xl:grid-cols-[1fr_365px] gap-y-10 gap-x-10 xl:gap-x-24">
+            <article class="doatkolom-post-content">
                 <?php the_content() ?>
-            </div>
-            <aside>
+            </article>
+            <aside class="-order-1 md:order-1">
                 <?php echo do_shortcode('[tocb]') ?>
             </aside>
         </div>
